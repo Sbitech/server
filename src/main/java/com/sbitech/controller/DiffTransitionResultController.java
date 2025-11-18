52e306c5bb546fcdcdbe2955f97f68be35bfe952
2oTA <zl2314241786@163.com>
2025-11-18 09:33:43 +0800

    修改视频上传获取的逻辑

--- src/main/java/com/sbitech/controller/DiffTransitionResultController.java
+++ src/main/java/com/sbitech/controller/DiffTransitionResultController.java
@@ -5,23 +5,43 @@ import com.sbitech.service.DiffTransitionResultService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @CrossOrigin
 @RestController
 @RequestMapping("/diffTransitionResult")
 public class DiffTransitionResultController {
+
+    private DiffTransitionResult diff;
 
     @Autowired
     private DiffTransitionResultService diffTransitionResultService;
 
     @PostMapping("/upDiffTransitionResult")
     public boolean upDiffTransitionResult(@RequestBody DiffTransitionResult diffTransitionResult) {
-        System.out.println(diffTransitionResult);
-        return diffTransitionResultService.upDiffTransitionResult(diffTransitionResult);
+
+        try {
+            diff=diffTransitionResult;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            String time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+            System.err.println(time+"  内容："+diff);
+        }
+        return true;
+//        System.out.println(diffTransitionResult);
+//         diffTransitionResultService.upDiffTransitionResult(diffTransitionResult);
     }
 
     @GetMapping("/getDiffTransitionResult")
     public DiffTransitionResult getDiffTransitionResult() {
-        return diffTransitionResultService.getDiffTransitionResult();
+        DiffTransitionResult polled=diff;
+        diff=null;
+        String time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        System.err.println(time+"  当前队列内容："+diff);
+        return polled;
+//        return diffTransitionResultService.getDiffTransitionResult();
     }
 
 }
